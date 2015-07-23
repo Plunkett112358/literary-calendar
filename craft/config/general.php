620ed60a6adb40623e5ff694f1332cc4503da8e3
Meghann Plunkett <plunkett.meghann@gmail.com>
2015-07-23 11:57:14 -0400

    first sync

---
+++ craft/config/general.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ */
+
+return array(
+
+	'*' => array(
+		 'omitScriptNameInUrls' => true,
+		 'resourceTrigger' => 'files',
+		 'useEmailAsUsername' => true,
+		 'autoLoginAfterAccountActivation' => true,
+		 'loginPath' => 'register'
+	),
+
+  '.dev' => array(
+    'devMode' => true,
+    'generateTransformsAfterPageLoad' => false,
+    'backupDbOnUpdate' => false,
+    'siteUrl' => 'http://literary-calendar.dev/',
+    'overridePhpSessionLocation' => true
+
+  ),
+
+  '.org' => array(
+  	'usePathInfo' => true,
+  	'generateTransformsAfterPageLoad' => false,
+  	'devMode' => false
+  )
+
+
+);
