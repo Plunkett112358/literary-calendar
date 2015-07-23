3f677c3891b128f3b68ddf06e12e5bd8a41e2d24
Tanner Welsh <tannerwelsh@gmail.com>
2015-07-23 12:54:39 -0400

    Use blog post version of db.php

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,22 +7,12 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 return array(
-
-  $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-	'*' => array(
-    'server' => $url["host"],
-    'user' => $url["user"],
-    'password' => $url["pass"],
-    'database' => substr($url["path"],1),
-    'tablePrefix' => 'craft',
-  ),
-
-  '.dev' => array(
-    'server' => 'localhost',
-    'user' => 'root',
-    'password' => 'root'
-  )
-
+  'server' => $url["host"],
+  'user' => $url["user"],
+  'password' => $url["pass"],
+  'database' => substr($url["path"],1),
+  'tablePrefix' => 'craft'
 );
