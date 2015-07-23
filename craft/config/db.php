5a784473eeb5b1e3c13963e87694c28cf7a5f3d4
Tanner Welsh <tannerwelsh@gmail.com>
2015-07-23 13:12:29 -0400

    Conditionally set database connection variables

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,12 +7,26 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+if(getenv("CLEARDB_DATABASE_URL")) {
+  $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-return array(
-  'server' => $url["host"],
-  'user' => $url["user"],
-  'password' => $url["pass"],
-  'database' => substr($url["path"],1),
-  'tablePrefix' => 'craft'
-);
+  return array(
+    'server' => $url["host"],
+    'user' => $url["user"],
+    'password' => $url["pass"],
+    'database' => substr($url["path"],1),
+    'tablePrefix' => 'craft'
+  );
+} else {
+  return array(
+    '*' => array(
+  		'tablePrefix' => 'craft',
+      'database' => 'literary_calendar_craft'
+    ),
+    '.dev' => array(
+      'server' => 'localhost',
+      'user' => 'root',
+      'password' => 'root'
+    )
+  )
+}
